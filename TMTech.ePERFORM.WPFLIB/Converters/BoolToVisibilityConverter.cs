eed05888a84dd9ada480f66cead40b7b6b526edd
Minghui Dai <minghuidai@hotmail.com>
2016-06-01 17:08:14 -0400

    added a BoolToVisibilityConverter class

---
+++ TMTech.ePERFORM.WPFLIB/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TMTech.Shared.WPFLIB.Converters
+{
+    /// <summary>
+    /// This class defines a value converter to inverse a boolean value.
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var visible = false;
+
+            bool.TryParse(value.ToString(), out visible);
+
+            if (visible)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+
+        }
+
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
