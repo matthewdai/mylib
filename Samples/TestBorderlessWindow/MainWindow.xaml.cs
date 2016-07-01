df5c80498e51b4b2be1035923e03898e72dcbf17
Minghui Dai <minghuidai@hotmail.com>
2016-07-01 14:39:27 -0400

    fixed a null error for MessageDialog

--- Samples/TestBorderlessWindow/MainWindow.xaml.cs
+++ Samples/TestBorderlessWindow/MainWindow.xaml.cs
@@ -30,5 +30,11 @@ namespace TestBorderlessWindow
         {
             MessageBox.Show("Testing");
         }
+
+        private void TestMessageDlg_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new MessageDialog();
+            dlg.ShowMsg("It's very easy to show this dialog as you only need to set the Visibility of the InputBox grid to visible. You then simply handle the Yes / No buttons and get the Input text from the TextBox. So instead of using code that requires ShowDialog(), you simply set the Visibility option to Visible.There are still some things to do in this example that we will handle in code - behind, like for example clearing the InputText box after handling the Yes / No Button clicks.");
+        }
     }
 }
