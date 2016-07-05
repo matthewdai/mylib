7e0b05126525352476a3eeb866ee14c857d3f96f
Minghui Dai <minghuidai@hotmail.com>
2016-07-05 14:15:30 -0400

    added TMTech.Shared.Commands project in the solution

---
+++ TMTech.Shared.Commands/Interfaces/IEditable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMTech.Shared.Commands.Interfaces
+{
+
+    /// <summary>
+    /// To use the commands binding, the control must implement the IEditable interface.
+    /// </summary>
+    public interface IEditable
+    {
+        void Copy();
+
+        bool CanCopy();
+
+        bool CanPaste();
+
+        void Paste();
+
+        bool CanDelete();
+
+        void Delete();
+
+    }
+}
