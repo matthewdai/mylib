9bf80e2de3ede273c64b9d7be5c2f5b2913490bb
Matthew Dai <matthew.dai@tokiomarinetech.com>
2017-03-01 17:12:51 -0500

    added feature for import and process file

--- Tools.FilenameChanger/Controls/WordProcessor.xaml.cs
+++ Tools.FilenameChanger/Controls/WordProcessor.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -55,5 +57,49 @@ namespace Tools.FilenameChanger.Controls
             return result.ToString();
         }
 
+
+
+        private void ImportFile(string filename, string outputFilename)
+        {
+
+            using (var sr = new StreamReader(filename, Encoding.GetEncoding(936)))
+            {
+                var writer = new StreamWriter(outputFilename);
+
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    line = line.Trim();
+
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+
+                writer.Close();
+            }
+
+        }
+
+
+        private void ProcessFile_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            dlg.CheckFileExists = true;
+            string filename;
+
+            if (dlg.ShowDialog() == true)
+            {
+                filename = dlg.FileName;
+            }
+            else
+                return;
+
+            // process file
+            ImportFile(filename, filename + ".txt");
+
+
+        }
     }
 }
