9b90a34659f23907043b4b3f1ed9d294b59c5967
Matthew Dai <matthew@tmtech.local>
2017-07-05 16:01:23 -0400

    try process

--- Samples/MutexDemo/App.xaml.cs
+++ Samples/MutexDemo/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,19 +18,17 @@ namespace MutexDemo
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            Start();
+            CheckInstance();
         }
 
 
-        static void Start()
+        static void CheckInstance()
         {
             using (SingleProgramInstance spi = new SingleProgramInstance("{4173DDF2-74F4-4927-9D35-9B826E916601}"))
             {
                 if (spi.IsSingleInstance)
                 {
-                    var win = new MainWindow();
-                    win.Show();
-                    spi.Dispose();
+                    Start();
                 }
                 else
                 {
@@ -39,6 +38,15 @@ namespace MutexDemo
                 }
             }
 
+        }
+
+
+
+        static void Start() {
+            //var process = Process.GetCurrentProcess();
+
+            var win = new MainWindow();
+            win.Show();
         }
 
 
