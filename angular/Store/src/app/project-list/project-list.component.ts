0fdfe3e319a19fadd528ba2bb7aa44637d7c70a0
Matthew Dai <matthew@tokiomarinetech.com>
2019-11-12 10:52:06 -0500

    try to use bootstrap modal

--- angular/Store/src/app/project-list/project-list.component.ts
+++ angular/Store/src/app/project-list/project-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
 import { Project } from '../shared/project';
 
 @Component({
@@ -7,6 +9,7 @@ import { Project } from '../shared/project';
   styleUrls: ['./project-list.component.css']
 })
 export class ProjectListComponent implements OnInit {
+  closeResult: string;
 
   projects: Project[] = [
     {id:1, name: "Project 1", owner: "John", dateCreated:new Date("1/1/2019"), detail: false}, 
@@ -19,5 +22,23 @@ export class ProjectListComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  // open(content) {
+  //   this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+  //     this.closeResult = `Closed with: ${result}`;
+  //   }, (reason) => {
+  //     this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+  //   });
+  // }
+
+  // private getDismissReason(reason: any): string {
+  //   if (reason === ModalDismissReasons.ESC) {
+  //     return 'by pressing ESC';
+  //   } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+  //     return 'by clicking on a backdrop';
+  //   } else {
+  //     return  `with: ${reason}`;
+  //   }
+  // }
 
 }
